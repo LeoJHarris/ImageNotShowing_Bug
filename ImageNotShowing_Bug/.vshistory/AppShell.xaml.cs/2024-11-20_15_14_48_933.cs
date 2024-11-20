648bf149c20671ea15ca32ead11250d541ee5381
Leonard Harris <leojharris@hotmail.com>
2024-11-20 15:38:22 +1300

    Add project files.

---
+++ ImageNotShowing_Bug/.vshistory/AppShell.xaml.cs/2024-11-20_15_14_48_933.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageNotShowing_Bug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ImageNotShowing_Bug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageNotShowing_Bug;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
